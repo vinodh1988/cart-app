ca18fac81334262cc18e1f78ab97403ed472b4bd
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-02 21:15:29 +0530

    functional-component

--- src/component/Reusable/Product.js
+++ src/component/Reusable/Product.js
@@ -2,19 +2,19 @@ import React from 'react';
 import '../styles/general.css';
 
 
-class Product extends React.Component{
-    render(){
-        const imgurl="http://localhost:8999/beautyapp/"+this.props.product.image;
+//class Product extends React.Component{
+    const Product =({product})=>{
+        const imgurl="http://localhost:8999/beautyapp/"+product.image;
         return(
          <div className="product">
-            <h5>{this.props.product.name}</h5>
+            <h5>{product.name}</h5>
             <div className="image-box">
                 <img src={imgurl} />
             </div>
-            <h6>Price: &#8377;{this.props.product.price}</h6>
+            <h6>Price: &#8377;{product.price}</h6>
         </div>
         )
     }
-}
+
 
 export default Product;
